0ecbc3170244e498312e58dc99639ed08474ed74
Samsonov Pavel <samsonovpavel@mail.ru>
2017-02-18 21:36:12 +0300

    Initial project

---
+++ Database/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Database
+//
+//  Created by Pavel Samsonov on 18.02.17.
+//  Copyright © 2017 Pavel Samsonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
